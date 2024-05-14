e45a3fd548784c6f5d1254a3f0c9b91cb99ed8fa
Jean Vuillet <vuillet433@hotmail.com>
2024-05-14 10:16:22 -0500

    working

--- After/src/App.js
+++ After/src/App.js
@@ -1,31 +1,26 @@
-
-import './app.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './Pages/HomePage/Home';
-import { EmployeeList } from './Pages/EmployeeListPage/EmployeeList';
-import { CreateEmployee } from './Pages/CreateEmployeePage/CreateEmployee';
-import { MainNav } from './Components/MainNavComp/MainNav';
-import { Provider } from 'react-redux';
-import {store} from './store.js'
+import "./app.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./Pages/HomePage/Home";
+import { EmployeeList } from "./Pages/EmployeeListPage/EmployeeList";
+import { CreateEmployee } from "./Pages/CreateEmployeePage/CreateEmployee";
+import { MainNav } from "./Components/MainNavComp/MainNav";
+import { Provider } from "react-redux";
+import { store } from "./store.js";
 
 function App() {
   return (
-
-    <div className='App'>
-
-    <Router>
-      <MainNav/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path ='/createEmployee' element={<CreateEmployee/>}/>
-        <Route path='/employeeList' element={<EmployeeList/>}/>
-      </Routes>
-    </Router>
-
+    <div className="App">
+      <Provider store={store}>
+        <Router>
+          <MainNav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/createEmployee" element={<CreateEmployee />} />
+            <Route path="/employeeList" element={<EmployeeList />} />
+          </Routes>
+        </Router>
+      </Provider>
     </div>
-    
-
-
   );
 }
 
--- After/src/store.js
+++ After/src/store.js
@@ -1,25 +1,45 @@
+import { configureStore } from '@reduxjs/toolkit';
+
 import { createSlice } from "@reduxjs/toolkit";
-import { configureStore } from "@reduxjs/toolkit";
 
 
 
 
 
-export const userSlice=createSlice({
-    name:'userList',
-    initialState:{},
-    reducers:{
-    addUser: (currentState, action)=>{ return {...currentState, ...action.payload }  }
-    }
-}
-)
-  export const getUser=(state)=>state.userList;
-const store=configureStore(
+
+export const firstSlice=createSlice(
     {
-        reducer:{
-            userList:userSlice.reducer}
-    },
-);
+        name:'myFirstSlice',
+        initialState:{
+            user:{},
+            props:{login:false}
+        },
+        reducers:{
+            setUser:(currentState,action)=>{{
+               const user= action.payload
+               return {...currentState,user:user}
+            }},
+            login: (currentState,action)=>{
+              {
+                const props={...currentState.props,login:action.payload}
+                return {...currentState,props:props}
+              }
+            }
+        }
+
+    }
+)
+export const storeGetUser = (state) => state && state.myFirstSlice.user;
+export const getLogin=(state)=>state && state.myFirstSlice.props.login
+
+const store = configureStore({
+  reducer:{
+    myFirstSlice:firstSlice.reducer,
+
+  },
+ 
+});
+  // Ajoutez ici d'autres options de configuration du store si nécessaire
 
 
-export {store}
+export {store};
