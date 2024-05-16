7ebe210c38fd9c48de78a667d5e845e9990e69e5
Jean Vuillet <vuillet433@hotmail.com>
2024-05-16 14:13:27 -0500

    tab

--- src/Pages/EmployeeListPage/EmployeeList.jsx
+++ src/Pages/EmployeeListPage/EmployeeList.jsx
@@ -2,59 +2,98 @@ import './EmployeeList.scss'
 import { useSelector } from 'react-redux'
 import { getUser } from '../../store'
 import DataTable from 'react-data-table-component';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 export function EmployeeList(){
 
+
+
     const users = useSelector(getUser);
-    const userList = users.user;
-    const [records, setRecords] = useState(userList);
+    const userList=users.user;
+    let [records, setRecords]=useState(userList)
 
 
-        // Lorsque userList est mis à jour, mettre à jour également records
+ function FindName(e){
 
 
-    function FindName(e) {
+    const dataFilterd=  userList.filter((element)=>{ return(
+element.lastName.toLowerCase().includes(e.target.value.toLowerCase())
+|| element.firstName.toLowerCase().includes(e.target.value.toLowerCase())
 
-        
-        const dataFiltered = userList.filter((element) => (
-            element.lastName.toLowerCase().includes(e.target.value.toLowerCase()) ||
-            element.firstName.toLowerCase().includes(e.target.value.toLowerCase())
-        ));
+)})
+setRecords(dataFilterd)
 
-        setRecords(dataFiltered);
-   
-        
-        if (!e.target.value) {
-            setRecords(userList);
-        }
-    }
+if(! e.target.value)
+   {setRecords(userList)}
 
-    const columns = [
-        {
-            name: 'FirstName',
-            selector: row => row.firstName,
-            sortable:true
-        },
-        {
-            name: 'LastName',
-            selector: row => row.lastName,
-            sortable:true
-        },
-        // Autres colonnes...
-    ];
+}
 
-    return (
+
+const columns = [
+	{
+		name: 'FirstName',
+		selector: row => row.firstName,
+        sortable:true
+	},
+	{
+		name: 'LastName',
+		selector: row => row.lastName,
+        sortable:true
+	},
+    {
+		name: 'DateOfBirth',
+		selector: row => row.dateOfBirth,
+        sortable:true
+	},
+    {
+		name: 'StartDate',
+		selector: row => row.startDate,
+        sortable:true
+	},
+    {
+		name: 'Street',
+		selector: row => row.street,
+        sortable:true
+	},
+    {
+		name: 'City',
+		selector: row => row.city,
+        sortable:true
+	},
+    {
+		name: 'State',
+		selector: row => row.state,
+        sortable:true
+	},
+    {
+		name: 'Zip Code',
+		selector: row => row.zip,
+        sortable:true
+	},
+    {
+		name: 'Departement',
+		selector: row => row.departement,
+        sortable:true
+	},
+
+];
+
+const data = userList;
+
+
+	return (
         <>
-            <div className="finder">
-                <label>Find User</label>
-                <input onChange={FindName}></input>
-            </div>
-            <DataTable
-                columns={columns}
-                data={records}
-            />
+        <div className="finder">
+            <label>find User</label>
+            <input onChange={FindName}></input>
+        </div>
+		<DataTable
+			columns={columns}
+			data={records}
+		/>
         </>
-    );
+	);
 };
+
