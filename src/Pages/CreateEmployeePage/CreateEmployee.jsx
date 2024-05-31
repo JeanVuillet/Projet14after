da4c9dc9cba9510eb649a0e2f2ded992017f334c
Jean Vuillet <vuillet433@hotmail.com>
2024-05-31 15:56:39 -0500

    double

--- src/Pages/CreateEmployeePage/CreateEmployee.jsx
+++ src/Pages/CreateEmployeePage/CreateEmployee.jsx
@@ -11,7 +11,7 @@ import React from "react";
 import Select from "react-select";
 import departmentList from "./../../Assets/departments.json";
 
-import MyModal from "mymodal4";
+import MyModal from "doublemodal";
 
 export function CreateEmployee() {
 
